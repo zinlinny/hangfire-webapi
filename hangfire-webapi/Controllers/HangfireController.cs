0d51dd7541a5f1fae3c543d4b35569a89b28a85f
zinlinny <zinlin.ny@gmail.com>
2024-04-02 14:58:55 +0630

    Initial commit

---
+++ hangfire-webapi/Controllers/HangfireController.cs
@@ -0,0 +1,57 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace hangfire_webapi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HangfireController : ControllerBase
+    {
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //    return Ok("Hello form hangfire webapi");
+        //}
+
+        [HttpPost]
+        [Route("[action]")]
+
+        public IActionResult Welcome()
+        {
+            var jobId = BackgroundJob.Enqueue(() => SendWelcomeEmail("Welcome to our app"));
+            return Ok($"Job Id = {jobId}.Welcome email sent to the user!");
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Discount()
+        {
+            int timeInSeconds = 30;
+            var jobId = BackgroundJob.Schedule(() => SendWelcomeEmail("Welcome to our app"), TimeSpan.FromSeconds(timeInSeconds));
+            return Ok($"Job Id = {jobId}.Discount email will be sent in {timeInSeconds} seconds!");
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult DatabaseUpdate()
+        {
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Database updated."), Cron.Minutely);
+            return Ok("Database check job initiated");
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Confirm()
+        {
+            int timeInSeconds = 30;
+            var parentJobId = BackgroundJob.Schedule(() => Console.WriteLine("You ask to be unsubscribed"), TimeSpan.FromSeconds(timeInSeconds));
+            BackgroundJob.ContinueJobWith(parentJobId, () => Console.WriteLine("You are unsubscribed"));
+            return Ok("Confirmation job created");
+        }
+        public void SendWelcomeEmail(string email)
+        {
+            Console.WriteLine(email);
+        }
+    }
+}
---
+++ hangfire-webapi/Startup.cs
@@ -0,0 +1,49 @@
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace hangfire_webapi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(x => x.UseSqlServerStorage(@"Data Source=DESKTOP-39EJGJO;Database=hangfire-webapi-db;TrustServerCertificate=True;User=sa;Password=P@ssw0rd"));
+            services.AddHangfireServer();
+            services.AddControllers();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseHangfireDashboard();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
